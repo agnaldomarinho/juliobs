6d5f1d0a802a0987254d60b144984421e6c227ba
Julio Silva <juliob91@gmail.com>
2012-06-01 17:30:31 -0300

    Arrumei mimetype do robots.txt e humans.txt mais outros pequenos ajustes

--- juliobs/urls.py
+++ juliobs/urls.py
@@ -1,12 +1,21 @@
+# -*-*- encoding: utf-8 -*-*-
 from django.conf.urls import patterns, include, url
-from views import *
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 from django.views.generic.base import RedirectView
+from django.http import HttpResponse
 
+from views import *
 from torrents.views import torrents
 from projetos.views import projetos, programas
 from ira.views import ira
+
+
+class PlainTextView(TemplateView):
+    """A plain text generic template view."""
+    def render_to_response(self, context, **kwargs):
+        return super(PlainTextView, self).render_to_response(
+                context, content_type='text/plain', **kwargs)
 
 admin.autodiscover()
 
@@ -15,23 +24,38 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^i18n/', include('django.conf.urls.i18n')),
     (r'^blog/(?P<post>.*)$',
-        RedirectView.as_view(url='http://blog.juliobs.com/%(post)s', permanent=True)),
+        RedirectView.as_view(url='http://blog.juliobs.com/%(post)s',
+            permanent=True)),
 
     (r'^$', homepage),
     (r'^music_library/$', music_library),
     (r'^horario/$', TemplateView.as_view(template_name='horario.html')),
     (r'^pgp/$', TemplateView.as_view(template_name='public_key.html')),
-    (r'^stuff-i-use/$', TemplateView.as_view(template_name='stuff_i_use.html')),
+    (r'^stuff-i-use/$',
+        TemplateView.as_view(template_name='stuff_i_use.html')),
     (r'^qrcode/$', TemplateView.as_view(template_name='qrcode.html')),
     (r'^torrents/$', torrents),
     (r'^projetos/$', projetos),
     (r'^projetos/(.*)$', programas),
     (r'^ira/$', ira),
     (r'^gallery/', include('imagestore.urls', namespace='imagestore')),
+)
 
-  # Robots & Humans
-    (r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt')),
-    (r'^humans.txt$', TemplateView.as_view(template_name='humans.txt')),
+urlpatterns += patterns('',
+    # Robots & Humans
+    (r'^humans.txt$', PlainTextView.as_view(template_name='humans.txt')),
+    url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *",
+        mimetype="text/plain")),
+    url(r'^favicon\.ico$',
+        RedirectView.as_view(url='/static/images/favicon.ico')),
+
+    # Para testar páginas de erro
     (r'^erro_500$', TemplateView.as_view(template_name='500.html')),
     (r'^erro_404$', TemplateView.as_view(template_name='404.html')),
+)
+
+# Não manda email sobre erros 404 que seguem alguns padrões
+import re
+IGNORABLE_404_URLS = (
+    re.compile(r'^/apple-touch-icon.*\.png$'),
 )
