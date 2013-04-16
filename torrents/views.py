848d8e269ae7fdbbd7b1f078e394e2e202391f83
Julio Silva <julio@juliobs.com>
2013-04-16 18:39:33 -0300

    Adiciona min_up e min_down

--- torrents/views.py
+++ torrents/views.py
@@ -29,6 +29,10 @@ class Rtorrent:
  
 @login_required
 def torrents(request):
+    min_up = 60 * 1024
+    min_down = 400 * 1024
+    max_up = 5000 * 1024
+    max_down = 5000 * 1024
     errors = []
     try:
         #r = Rtorrent("casa.juliobs.com", "80", "http")
@@ -37,12 +41,17 @@ def torrents(request):
         if request.method == 'POST':
             if request.POST['up']:
                 new_up = int(request.POST['up']) * 1024
-                if new_up >= 30 * 1024 or new_up == 0:
+                if new_up == 0 or new_up > max_up:
+                    r.set_up_rate(max_up)
+                elif new_up >= min_up:
                     r.set_up_rate(new_up)
 
             if request.POST['down']:
                 new_down = int(request.POST['down']) * 1024
-                r.set_down_rate(new_down)
+                if new_down == 0 or new_down > max_down:
+                    r.set_down_rate(max_down)
+                elif new_down >= min_down:
+                    r.set_down_rate(new_down)
 
         up = int(r.get_up_rate()) / 1024
         down = int(r.get_down_rate()) / 1024
